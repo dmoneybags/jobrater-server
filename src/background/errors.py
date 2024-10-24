d8f3bbc37a160cfb49ea8a2a307924f2c7d3a622
Daniel DeMoney <weston@MacBook-Air-7.home>
2024-10-24 10:48:42 -0700

    Added free user data class and route to get data

--- src/background/errors.py
+++ src/background/errors.py
@@ -7,3 +7,12 @@ class DuplicateUserJob(Exception):
         
     def __str__(self):
         return self.message
+
+class NoFreeRatingsLeft(Exception):
+    """when a user attempts to use a resume rating when they have ran out of free ones"""
+    
+    def __init__(self, message="Error, user has already used all free resume ratings"):
+        self.message = message
+        
+    def __str__(self):
+        return self.message
